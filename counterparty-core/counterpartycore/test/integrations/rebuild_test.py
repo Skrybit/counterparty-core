fdb984a9ecbf0c6db960f07e89a6ca34729a4d7a
Ouziel Slama <ouziel@gmail.com>
2026-01-14 17:49:43 +0000

    fix rebuild test

--- counterparty-core/counterpartycore/test/integrations/rebuild_test.py
+++ counterparty-core/counterpartycore/test/integrations/rebuild_test.py
@@ -2,5 +2,5 @@ from reparsetest import prepare
 
 
 def test_rebuild():
-    sh_counterparty_server, _db_file, _api_url = prepare("testnet4")
+    sh_counterparty_server, _server_args, _db_file, _api_url = prepare("testnet4")
     sh_counterparty_server("rebuild", "--rebuild-state-db")
